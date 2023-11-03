0a12b41116b26fcc7911bce8b5f4ee1bba687bc3
Ignacio Garcia <ignaciogarciag16@gmail.com>
2023-11-03 17:33:53 +0100

    test_mysin & test_myarcsin functions (incomplete)

--- test_haversine.py
+++ test_haversine.py
@@ -1,19 +1,32 @@
 from myhaversine import myhaversine, mysqrt, mycos, myarcsin, mysin
+import numpy as np
 
 def test_myhaversine():
-
+    pass
 
 
 def test_mysqrt():
-
+    pass
 
 
 def test_mycos():
-    
+    pass
     
 
 def test_myarcsin():
-    
-    
+    assert myarcsin(-300) == -90
+    assert myarcsin(-1) == -90
+    assert myarcsin(0) == 0
+    assert myarcsin(1) == 90
+    assert myarcsin(300) == 90
     
 def test_sin():
+    assert mysin(90) == 1
+    assert mysin(30) == 0.5
+    assert mysin(180) == 0
+    assert mysin(270) == -1
+
+test_myarcsin()
+
+test_sin()
+
