c7be83563e97845e687860ed2f7dfe62a1bbfab9
Dominic Welsh <domwelsh@gmail.com>
2023-11-06 10:26:04 +0000

    Added assertions to test_mysqrt() and test_mycos() in test_haversine

--- test_haversine.py
+++ test_haversine.py
@@ -6,12 +6,20 @@ def test_myhaversine():
 
 
 def test_mysqrt():
-    pass
+    assert mysqrt(-10) == 0
+    assert -0.01 <= mysqrt(0) <= 0.01
+    assert mysqrt(1) == 1
+    assert 4.999 <= mysqrt(25) <= 5.001
+    assert 54.771 <= mysqrt(3000) <= 54.773
 
 
 def test_mycos():
-    pass
-    
+    assert mycos(0) == 1
+    assert mycos(90) == 0
+    assert 0.706 <= mycos(45) <= 0.708
+    assert -0.708 <= mycos(135) <= -0.706
+    assert mycos(180) == -1
+    assert -0.501 <= mycos(3000) <= -0.499
 
 def test_myarcsin():
     assert myarcsin(-300) == -90
@@ -26,7 +34,7 @@ def test_sin():
     assert mysin(180) == 0
     assert mysin(270) == -1
 
-test_myarcsin()
+# test_myarcsin()
 
-test_sin()
+# test_sin()
 
